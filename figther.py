89779696e663ff318b114ccfc5fa1e0ca3b337b3
Lefaure <n.lefaure@netia.com>
2022-11-16 09:36:19 +0100

    implémentation des event de mouvement, gauche droite, saut. Gestion des limites de l'écran (les personnages ne peuvent plus sortir de la surface d'affichage)

--- figther.py
+++ figther.py
@@ -4,32 +4,99 @@ import pygame
 class Fighter():
     # Constructor
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 50, 150))
+        self.velocity_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
+        
+    '''    
+    def load_images(self, sprite_sheet, animation_steps):
+        for _ in range(animation):
+    '''    
+ 
+        
     # Movement handler
-
-    def move(self, screen_width):
+    def move(self, screen_width, screen_height, floor_height, surface, target):
         SPEED = 10
+        GRAVITY = 2
         dx = 0
         dy = 0
 
         # listen to key press
         key = pygame.key.get_pressed()
 
-        # movement
-        if key[pygame.K_q]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = +SPEED
+        # All movement bellow is avaible if not attacking
+        if self.attacking == False:
+            # movement
+                # left (x axis)
+            if key[pygame.K_q]:
+                dx = -SPEED
+                # rigth (x axis)
+            if key[pygame.K_d]:
+                dx = +SPEED
+                # jump up / down gravity (y axis)
+            if key[pygame.K_z] and self.jump == False:
+                self.velocity_y = -30
+                self.jump = True
+                # attacks
+            if key[pygame.K_v] or key[pygame.K_b]:
+                
+                self.attack(surface, target)
+                # Now get the precise attack being used
+                if key[pygame.K_v]:
+                    self.attack_type = 1
+                if key[pygame.K_b]:
+                    self.attack_type = 2
+
+        # apply gravity 
+        self.velocity_y += GRAVITY
+        dy += self.velocity_y
 
         # keep player onto the screen
+        # Left side of th screen
         if self.rect.left + dx < 0:
             dx = -self.rect.left
+        # Right side of the screen, 
         if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
+        # Up & Down
+        if self.rect.bottom + dy > screen_height - floor_height:
+            self.velocity_y = 0
+            # The player is on the floor so he can jump again
+            self.jump = False
+            dy = screen_height - floor_height - self.rect.bottom
+        
+        
+        # Keep player facing each other
+        if target.rect.centerx > self.rect.centerx:
+            # initial case for player one
+            self.flip = False
+        else:
+            # if player one go over player two position
+            self.flip = True
+            
         # update player position
         self.rect.x += dx
         self.rect.y += dy
 
+    # Attack Method
+    def attack(self, surface, target):
+        self.attacking = True
+        # self.rect.centerx - (2 * self.rect.width * self.flip)
+        # Help determine wich side should be attacked depending on self.flip
+        # If True then draw the rectangle on the left side of the player
+        attack_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        # check for collision between attack and the other player
+        if attack_rect.colliderect(target.rect):
+            print("ouchy Baby")
+            target.health -= 10
+            print(target.health)
+        pygame.draw.rect(surface, (255, 50, 0), attack_rect)
+        self.attacking = False
+        
     # Draw figther as rectangle
     def drawFigther(self, surface):
         pygame.draw.rect(surface, (255, 0, 250), self.rect)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ pygame.init()
 # Game window variable
 win_x = 1000
 win_y = 500
+bgd_floor = 267
+floor_height = 82
 # Screen size definition
 screen = pygame.display.set_mode((win_x, win_y))
 
@@ -23,10 +25,31 @@ pygame.display.set_caption('- Moon Nigth Rising -')
 clock = pygame.time.Clock()
 fps = 60
 
+# Colors definition
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+
 # Load image BackGround to the size of screen: Surface
 background_img = pygame.image.load(
     'assets/images/background/background.png').convert_alpha()
 
+# Load figthers sprite
+# Kenji
+kenji_idle_sprite = pygame.image.load('assets/images/Kenji/Idle.png').convert_alpha()
+kenji_attack1_sprite = pygame.image.load('assets/images/Kenji/Attack1.png').convert_alpha()
+# Samurai Mack
+samuraiMack_idle_sprite = pygame.image.load('assets/images/samuraiMack/Idle.png').convert_alpha()
+samuraiMack_attack1_sprite = pygame.image.load('assets/images/samuraiMack/Attack1.png').convert_alpha()
+
+# Define number of sprite in an animation
+kenji_idle_steps = 4
+kenji_attack1_steps = 4
+
+
+samuraiMack_idle_steps = 8
+samuraiMack_attack1_steps = 6
 
 # Draw background function,
 # Scale the image on the 'screen' size
@@ -35,11 +58,18 @@ def drw_bg():
     background_scale = pygame.transform.scale(background_img, (win_x, win_y))
     screen.blit(background_scale, (0, 0))
 
+# Draw players life bars
+def drw_life_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, white, (x - 3, y - 3, 406, 36))
+    pygame.draw.rect(screen, red, (x, y, 400, 30))
+    pygame.draw.rect(screen, blue, (x, y, 400 * ratio, 30))
+    
+
 
 # Instances of Figther()
-figther_1 = Fighter(200, 267)
-figther_2 = Fighter(700, 267)
-
+figther_1 = Fighter(200, bgd_floor, samuraiMack_idle_sprite, samuraiMack_idle_steps)
+figther_2 = Fighter(500, bgd_floor, kenji_idle_sprite, kenji_idle_steps)
 
 # Game loop
 run = True
@@ -49,12 +79,18 @@ while run:
     clock.tick(fps)
     # Draw background
     drw_bg()
+    # Draw Health Bars
+    drw_life_bar(figther_1.health, 20, 20)
+    drw_life_bar(figther_2.health, 580, 20)
     # manage figthers movement
-    figther_1.move(win_x)
-    figther_2.move(win_x)
+    figther_1.move(win_x, win_y, floor_height, screen, figther_2)
+    figther_2.move(win_x, win_y, floor_height, screen, figther_1)
+
     # Draw Figthers
     figther_1.drawFigther(screen)
     figther_2.drawFigther(screen)
+    
+    
     # Add Title
     sysFont = pygame.font.SysFont('arial', 42, True)
     rendered = sysFont.render('Moon Nigth Rising', 0, (255, 100, 100))
