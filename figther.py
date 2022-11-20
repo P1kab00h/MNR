098403272f223f315eabe01031acfbf7d101ece7
P1kab00H <nicolas.lefaure@gmail.com>
2022-11-20 18:36:49 +0100

    idle, runing, jumping, attacking added, need to test go further in update_sprite(), in order to manage the frame_index in a better way (currently KO)

--- figther.py
+++ figther.py
@@ -3,34 +3,39 @@ import pygame
 
 class Fighter():
     # Constructor
-    def __init__(self, x, y, data, idle_sprite, idle_steps):
+    def __init__(self, x, y, flip, data, sprite_sheet, sprite_steps):
         self.size = data[0]
-        self.image_scale = data[2]
-        self.offset = data[3]
-        self.flip = False
-        self.animation_list = self.load_images(idle_sprite, idle_steps)
-        self.action = 0 # 0 -> idle ; 1 -> run ; 2 -> jump ; 3 -> attack1 ; 4 -> attack2 ; 5 -> hit ; 6 -> death
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, sprite_steps)
+        self.action = 3 ###### #0-> atck1, #1-> atck2, #2-> death, #3-> idle, #4-> jump, #5-> run, #6 ->take hit
         self.frame_index = 0
         self.image = self.animation_list[self.action][self.frame_index]
+        self.update_sprite_time = pygame.time.get_ticks() # give to a Figther() instance a time stamp at the creation
         self.rect = pygame.Rect((x, y, 50, 150))
         self.velocity_y = 0
+        self.runing = False
         self.jump = False
         self.attacking = False
         self.attack_type = 0
         self.health = 100
         
 
-    def load_images(self, idle_sprite, idle_steps):
-        # ectrating all the images from on sprite animation
+    def load_images(self, sprite_sheet, sprite_steps):
+        # extracting all the images from on sprite animation
+        # y = 0
         animation_list = []
-        temp_img_list = []
-        for x in range(idle_steps):
-            # 10316, to iterate on each single sprite
-            temp_img = idle_sprite.subsurface(x * self.size, 0, self.size, self.size)
-            temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
-        animation_list.append(temp_img_list)
-        # print(animation_list)
+        for y, animation in enumerate(sprite_steps):
+            temp_img_list = []
+            for x in range(animation):
+                # to iterate on each single sprite
+                temp_img = sprite_sheet.subsurface(self.size * x, self.size * y, self.size, self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+            print(animation_list)
         return animation_list
+            # y += 1 ==> similaire au comportement obtenu avec enumerate
 
 
         
@@ -40,6 +45,9 @@ class Fighter():
         GRAVITY = 2
         dx = 0
         dy = 0
+        # reset self.runing => go back to idle sprite, self.action = 3
+        self.runing = False
+        self.action = 3
 
         # listen to key press
         key = pygame.key.get_pressed()
@@ -50,12 +58,17 @@ class Fighter():
                 # left (x axis)
             if key[pygame.K_q]:
                 dx = -SPEED
+                self.runing = True
+                self.action = 5
                 # rigth (x axis)
             if key[pygame.K_d]:
                 dx = +SPEED
+                self.runing = True
+                self.action = 5
                 # jump up / down gravity (y axis)
             if key[pygame.K_z] and self.jump == False:
                 self.velocity_y = -30
+                self.action = 4
                 self.jump = True
                 # attacks
             if key[pygame.K_v] or key[pygame.K_b]:
@@ -64,8 +77,10 @@ class Fighter():
                 # Now get the precise attack being used
                 if key[pygame.K_v]:
                     self.attack_type = 1
+                    self.action = 0
                 if key[pygame.K_b]:
                     self.attack_type = 2
+                    self.action = 1
 
         # apply gravity 
         self.velocity_y += GRAVITY
@@ -98,10 +113,25 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
+    # Dealing with sprites updates
+    def update_sprite(self):
+        # create a cooldown value in order to change sprite each 100 ms
+        animation_cooldown = 100
+        # to 'animate' the Figther we gonna need to go thru the self.frame_index, doing so will change the self.image for each new index reached
+        self.image = self.animation_list[self.action][self.frame_index]
+        # if statement to check if enough time has passed since last update
+        if pygame.time.get_ticks() - self.update_sprite_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_sprite_time = pygame.time.get_ticks()
+        # Check if the sprite array has reach the end, if so go back to the beginning
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
     # Attack Method
     def attack(self, surface, target):
         self.attacking = True
-        # self.rect.centerx - (2 * self.rect.width * self.flip)
+        # ==> self.rect.centerx - (2 * self.rect.width * self.flip)
         # Help determine wich side should be attacked depending on self.flip
         # If True then draw the rectangle on the left side of the player
         attack_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
@@ -113,7 +143,7 @@ class Fighter():
         pygame.draw.rect(surface, (255, 50, 0), attack_rect)
         self.attacking = False
         
-    # Draw figther as rectangle
+    # Draw figther as rectangle(old), now draw the figther and check if he is facing the rigth side
     def drawFigther(self, surface):
         img = pygame.transform.flip(self.image, self.flip, False)
         # pygame.draw.rect(surface, (255, 0, 250), self.rect)
--- main.py
+++ main.py
@@ -32,38 +32,43 @@ blue = (0, 0, 255)
 white = (255, 255, 255)
 
 # Fighters variables
-kenji_attack1_size = 200
-kenji_idle_size = 200
+kenji_size = 200
 kenji_scale = 2
 kenji_offset = [89, 53]
-kenji_data = [kenji_attack1_size, kenji_idle_size, kenji_scale, kenji_offset]
+kenji_data = [kenji_size, kenji_scale, kenji_offset]
 
-samuraiMack_attack1_size = 200
-samuraiMack_idle_size = 200
+samuraiMack_size = 200
 samuraiMack_scale = 2
 samuraiMack_offset = [85, 47]
-samuraiMack_data = [samuraiMack_attack1_size, samuraiMack_idle_size, samuraiMack_scale, samuraiMack_offset]
+samuraiMack_data = [samuraiMack_size, samuraiMack_scale, samuraiMack_offset]
 
 
 # Load image BackGround to the size of screen: Surface
 background_img = pygame.image.load(
     'assets/images/background/background.png').convert_alpha()
 
-# Load figthers sprite
-# Kenji
-kenji_idle_sprite = pygame.image.load('assets/images/Kenji/Idle.png').convert_alpha()
-kenji_attack1_sprite = pygame.image.load('assets/images/Kenji/Attack1.png').convert_alpha()
-# Samurai Mack
-samuraiMack_idle_sprite = pygame.image.load('assets/images/samuraiMack/Idle.png').convert_alpha()
-samuraiMack_attack1_sprite = pygame.image.load('assets/images/samuraiMack/Attack1.png').convert_alpha()
+################### Load figthers sprite
+#### Kenji
+kenji_full_sprite = pygame.image.load('assets/images/Kenji/kenji-full-sprites.png').convert_alpha()
+#kenji_idle_sprite = pygame.image.load('assets/images/Kenji/Idle.png').convert_alpha()
+#kenji_attack1_sprite = pygame.image.load('assets/images/Kenji/Attack1.png').convert_alpha()
+#kenji_run_sprite = pygame.image.load('assets/images/Kenji/Run.png').convert_alpha()
+##### Samurai Mack
+samuraiMack_full_sprite = pygame.image.load('assets/images/samuraiMack/samuraiMack-full-sprites.png').convert_alpha()
+#samuraiMack_idle_sprite = pygame.image.load('assets/images/samuraiMack/Idle.png').convert_alpha()
+#samuraiMack_attack1_sprite = pygame.image.load('assets/images/samuraiMack/Attack1.png').convert_alpha()
+#samuraiMack_run_sprite = pygame.image.load('assets/images/samuraiMack/Run.png').convert_alpha()
 
-# Define number of sprite in an animation
-kenji_idle_steps = 4
-kenji_attack1_steps = 4
-
-
-samuraiMack_idle_steps = 8
-samuraiMack_attack1_steps = 6
+##### Define number of sprite in an animation
+# atck1, atck2, death, idle, jump, run, take hit
+kenji_full_sprites_steps = [4, 4, 8, 4, 2, 8, 3]
+#kenji_idle_steps = 4
+#kenji_attack1_steps = 4
+#kenji_run_steps = 8
+samuraiMack_full_sprites_steps = [6, 6, 6, 8, 2, 8, 4]
+#samuraiMack_idle_steps = 8
+#samuraiMack_attack1_steps = 6
+#samuraiMack_run_steps = 8
 
 # Draw background function,
 # Scale the image on the 'screen' size
@@ -82,10 +87,10 @@ def drw_life_bar(health, x, y):
 
 
 # Instances of Figther()
-figther_1 = Fighter(200, bgd_floor, samuraiMack_data, samuraiMack_idle_sprite, samuraiMack_idle_steps)
-#figther_1 = Fighter(200, bgd_floor)
-figther_2 = Fighter(500, bgd_floor, kenji_data, kenji_idle_sprite, kenji_idle_steps)
-#figther_2 = Fighter(500, bgd_floor)
+figther_1 = Fighter(200, bgd_floor, False, samuraiMack_data, samuraiMack_full_sprite, samuraiMack_full_sprites_steps)
+
+# figther_2 = Fighter(500, bgd_floor, True, kenji_data, kenji_full_sprite, kenji_full_sprites_steps)
+figther_2 = Fighter(500, bgd_floor, True, samuraiMack_data, samuraiMack_full_sprite, samuraiMack_full_sprites_steps)
 
 # Game loop
 run = True
@@ -102,6 +107,8 @@ while run:
     figther_1.move(win_x, win_y, floor_height, screen, figther_2)
     figther_2.move(win_x, win_y, floor_height, screen, figther_1)
 
+    figther_1.update_sprite()
+    figther_2.update_sprite()
     # Draw Figthers
     figther_1.drawFigther(screen)
     figther_2.drawFigther(screen)
